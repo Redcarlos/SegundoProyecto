f49c2061ef06f496aa0e52e2bbdec7ab3e40408b
Redcarlos <39527912+Redcarlos@users.noreply.github.com>
2021-04-30 12:47:38 -0500

    materialize

--- src/app/App.js
+++ src/app/App.js
@@ -3,8 +3,37 @@ import React, { Component } from 'react';
 class App extends Component{
     render(){
         return (
-            <div className="container">
-                <h1>hola mundo</h1>
+            <div>
+                <nav className="light-blue darken-4">
+                    <div className="container">
+                        <a className="brand-logo" href="/">hola</a>
+                    </div>
+                </nav>
+                <div className="container">
+                    <div className="row">
+                        <div className="col s5">
+                            <div className="card">
+                                <div className="card-content">
+                                    <form>
+                                        <div className="row">
+                                            <div className="input-field col s12">
+                                                <input type="text" placeholder="Titulo de la tarea"/>
+                                            </div>
+                                        </div>
+                                        <div className="row">
+                                            <div className="input-field col s12">
+                                                <textarea placeholder="Descripcion" className="materialize-textarea"></textarea>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col s7">
+
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
