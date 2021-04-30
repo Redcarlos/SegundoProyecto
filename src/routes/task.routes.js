33ec39c35d9721748e211c2f0fefd3ebf066e12c
Redcarlos <39527912+Redcarlos@users.noreply.github.com>
2021-04-30 02:39:30 -0500

    d

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -2,10 +2,19 @@ const express = require('express');
 const router = express.Router();
 //'Router' un metodo para devolver un objeto para ingresar rutas 
 
-router.get('/', (req, res) => {
-    res.json({
-        status: 'API Works!'
-    });
+const Task = require('../models/task');
+
+router.get('/', async (req, res) => {
+    const tasks = await Task.find();
+    res.json(tasks);
+
 });
 
+router.post('/', async (req, res) =>{
+    const { title, description } = req.body;
+    const task = new Task({title, description});
+    await task.save();
+    console.log(task);
+    res.json({status: 'Task Saved'});
+});
 module.exports = router;
